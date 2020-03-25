432b1f87f5f677fffa5ba0d5d4e5b61b885b7c14
Brendon Daugherty <brendond@fb.com>
2020-03-24 19:08:13 -0700

    Setup starting log

--- src/main.rs
+++ src/main.rs
@@ -408,7 +408,31 @@ impl GameField {
     }
 }
 
+
+use log::{Record, Level, Metadata, SetLoggerError, LevelFilter};
+
+struct SimpleLogger;
+
+impl log::Log for SimpleLogger {
+    fn enabled(&self, metadata: &Metadata) -> bool {
+        metadata.level() <= Level::Info
+    }
+
+    fn log(&self, record: &Record) {
+        if self.enabled(record.metadata()) {
+            println!("{} - {}", record.level(), record.args());
+        }
+    }
+
+    fn flush(&self) {}
+}
+
+static LOGGER: SimpleLogger = SimpleLogger;
+
 fn main() -> Result<()> {
+    log::set_logger(&LOGGER)
+        .map(|()| log::set_max_level(LevelFilter::Info));
+    
     let game_identities = Identity::Ambassador
         | Identity::Assassin
         | Identity::Contessa
